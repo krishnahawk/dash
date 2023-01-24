1580a3e80f698cedf0ab42c28284d183b761c775
Krishna Hawk <hi@krishnahawk.com>
2023-01-24 00:51:41 -1000

    Fixed mistype

--- clock.js
+++ clock.js
@@ -100,7 +100,6 @@ $( '#40button' ).click( function() {
 $( '#50button' ).click( function() {
     startTimer(50);
 });
-if ()
 
 	// find the amount of "seconds" between now and target
 	var current_date = new Date().getTime();
